05d519836a6460128efab0ed9dabdc6f3ea3f7bc
Natasari <penitasari93@gmail.com>
2014-10-23 21:19:09 +0700

    Update send2.c

--- send2.c
+++ send2.c
@@ -10,8 +10,9 @@
 
 #define BUF 1024
 
+
 void encodeblock( unsigned char in[], char b64str[], int len );
-void b64_encode(char *clrstr, char *dst);
+void b64_encode(char *clrstr, char *b64dst);
 
 
 int main(int argc, char *argv[]){
@@ -143,6 +144,42 @@ int main(int argc, char *argv[]){
 
 char b64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
+/* decodeblock - decode 4 '6-bit' characters into 3 8-bit binary bytes */
+void decodeblock(unsigned char in[], char *clrstr) {
+  unsigned char out[4];
+  out[0] = in[0] << 2 | in[1] >> 4;
+  out[1] = in[1] << 4 | in[2] >> 2;
+  out[2] = in[2] << 6 | in[3] >> 0;
+  out[3] = '\0';
+  strncat(clrstr, out, sizeof(out));
+}
+
+void b64_decode(char *b64src, char *clrdst) {
+  int c, phase, i;
+  unsigned char in[4];
+  char *p;
+
+  clrdst[0] = '\0';
+  phase = 0; i=0;
+  while(b64src[i]) {
+    c = (int) b64src[i];
+    if(c == '=') {
+      decodeblock(in, clrdst); 
+      break;
+    }
+    p = strchr(b64, c);
+    if(p) {
+      in[phase] = p - b64;
+      phase = (phase + 1) % 4;
+      if(phase == 0) {
+        decodeblock(in, clrdst);
+        in[0]=in[1]=in[2]=in[3]=0;
+      }
+    }
+    i++;
+  }
+}
+
 /* encodeblock - encode 3 8-bit binary bytes as 4 '6-bit' characters */
 void encodeblock( unsigned char in[], char b64str[], int len ) {
     unsigned char out[5];
@@ -156,7 +193,7 @@ void encodeblock( unsigned char in[], char b64str[], int len ) {
 }
 
 /* encode - base64 encode a stream, adding padding if needed */
-void b64_encode(char *clrstr, char *dst) {
+void b64_encode(char *clrstr, char *b64dst) {
   unsigned char in[3];
   int i, len = 0;
   int j = 0;
